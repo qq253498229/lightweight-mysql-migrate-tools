ef280577aaa41dee23cf03fb3bb188fb7ee04901
大胃王 <253498229@qq.com>
2019-12-03 20:36:17 +0800

    update

--- mysql-migrate/src/main/java/cn/codeforfun/migrate/core/utils/DbUtil.java
+++ mysql-migrate/src/main/java/cn/codeforfun/migrate/core/utils/DbUtil.java
@@ -1,19 +1,16 @@
 package cn.codeforfun.migrate.core.utils;
 
 import cn.codeforfun.migrate.core.exception.DatabaseConnectException;
-import cn.codeforfun.migrate.core.exception.SqlExecuteException;
-import com.alibaba.druid.pool.DruidDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbutils.BasicRowProcessor;
 import org.apache.commons.dbutils.BeanProcessor;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 
-import javax.sql.DataSource;
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -52,72 +49,8 @@ public class DbUtil {
         return connection;
     }
 
-    public static List<String> executeSql(Connection connection, String sql) {
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(sql);
-            List<String> tableNameList = new ArrayList<>();
-            while (rs.next()) {
-                int columnCount = rs.getMetaData().getColumnCount();
-                String tableName = rs.getString(columnCount);
-                tableNameList.add(tableName);
-            }
-            return tableNameList;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            log.error(e.getMessage());
-            throw new SqlExecuteException(e.getMessage());
-        }
-    }
-
-    public static DataSource getDataSource(String url, String username, String password) {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        dataSource.setMaxWait(5000);
-        try {
-            dataSource.getConnection();
-        } catch (SQLException e) {
-            throw new RuntimeException("数据库连接失败");
-        }
-        return dataSource;
-    }
-
-    public static Object query(DataSource dataSource, String sql) throws SQLException {
-        QueryRunner runner = new QueryRunner(dataSource);
-        ResultSetHandler<Object> resultList = rs -> {
-            if (!rs.next()) {
-                return null;
-            }
-
-            ResultSetMetaData meta = rs.getMetaData();
-            int cols = meta.getColumnCount();
-            Object[] result = new Object[cols];
-
-            for (int i = 0; i < cols; i++) {
-                result[i] = rs.getObject(i + 1);
-            }
-
-            return result;
-        };
-        return runner.query(sql, resultList);
-    }
-
-    public static <T> List<T> queryList(DataSource dataSource, String sql, Class<T> clazz) throws SQLException {
-        QueryRunner runner = new QueryRunner(dataSource);
-        ResultSetHandler<List<T>> resultList = rs -> {
-            if (!rs.next()) {
-                return null;
-            }
-            ResultSetMetaData meta = rs.getMetaData();
-            int cols = meta.getColumnCount();
-            List<T> result = new ArrayList<>();
-            for (int i = 0; i < cols; i++) {
-                result.add(rs.getObject(i + 1, clazz));
-            }
-            return result;
-        };
-        return runner.query(sql, resultList);
+    public static void execute(Connection connection, String sql) throws SQLException {
+        QueryRunner runner = new QueryRunner();
+        runner.update(connection, sql);
     }
 }
