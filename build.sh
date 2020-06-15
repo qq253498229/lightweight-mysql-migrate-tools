c42e3a7e2b24b5a7c96dd2fff9aa6a1849e1dea3
大胃王 <253498229@qq.com>
2020-06-15 10:51:25 +0800

    更新文档样式

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 #asciidoctor -a stylesheet=docs/css.css -o docs/index.html index.adoc
 #asciidoctor -o docs/index.html README.adoc
-asciidoctor README.adoc -a stylesheet=docs/css/css.css -o ./docs/index.html
+#asciidoctor README.adoc -a stylesheet=docs/css/css.css -o ./docs/index.html
+rm -rf docs
+mkdir -p docs/images
+cp -r css/images/ docs/images
+asciidoctor README.adoc -o ./docs/index.html
